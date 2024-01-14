44e2e3f0320c9d6e21e9e3f4cf6849b2adab8f47
thanvu <vutranthanh@vnpt.vn>
2024-01-14 19:55:23 +0700

    update

--- app.js
+++ app.js
@@ -7,26 +7,26 @@ const cors = require('cors')
 const app = express()
 const port = 5000
 let counter = 0
+
 app.use(express.json())
-const FormData = require('form-data');
-const urlLocal = 'http://localhost:5000'
+app.use('/uploads', express.static('uploads'))
+app.use(cors())
+const FormData = require('form-data')
 const urlServer = 'http://trum99.ddns.net:5000'
 const staticFolderPath = path.join(__dirname, 'static')
 const countFilePath = path.join(staticFolderPath, 'count.txt')
-const currentFilePath = path.join(staticFolderPath, 'current_phone.txt')
+// const currentFilePath = path.join(staticFolderPath, 'current_phone.txt')
 const newPhonetFilePath = path.join(staticFolderPath, 'new_phone.txt')
 
 app.get('/getAPI', async (req, res) => {
+    counter = 0
+    let listAllPhones = []
     while (true) {
-        const response = await axios.get(
-            'https://www.firefox.fun/yhapi.ashx?act=getPhone&token=63e825cd6b0512c1eb5b85a0bbcb3b24_18800&iid=1008&did=vnm-1008-99991',
-        )
+        const response = await axios.get('http://www.worldcode.win/yhapi.ashx?act=getPhone&token=d0ba68cd9a3cce002431d60f1dcb8df0_347&iid=1000&country=vnm')
         const responseData = response.data
-        console.log('responseData: ', responseData)
         if (responseData) {
             if (responseData.trim() === '0|-1') {
-                console.log('Stopping')
-                res.send('Đã lấy hết phone')
+                res.json(1)
                 break
             }
             const data = responseData.split('|')
@@ -35,15 +35,18 @@ app.get('/getAPI', async (req, res) => {
             const phone = data[data.length - 1]
             const id = data[1]
 
-            axios.get(`http://www.firefox.fun/yhapi.ashx?act=addBlack&token=b7c94daad5e3dd71ffca9298976ec0d4_3&pkey=${id}&reason=used`)
+            axios.get(`http://www.worldcode.win/yhapi.ashx?act=addBlack&token=d0ba68cd9a3cce002431d60f1dcb8df0_347&pkey=${id}&reason=used`)
 
             const filePath = path.join(staticFolderPath, `list_phone_${port}.txt`)
+            const filePortPath = path.join(staticFolderPath, `status_${port}.txt`)
+            fs.writeFileSync(filePortPath, '0,0')
 
             let phoneArray = []
             phoneArray.push(phone.toString())
             for (let i = 0; i < 11; i++) {
                 let newPhone = phone + `${i}`
                 phoneArray.push(newPhone.toString())
+                listAllPhones.push(`${port}-${newPhone.toString()}`)
                 fs.writeFileSync(filePath, phoneArray.join(','), 'utf8')
             }
         }
@@ -53,10 +56,9 @@ app.get('/getAPI', async (req, res) => {
 // Láy phone đã active
 app.get('/get-list-phone-active', (req, res) => {
     const phoneList = []
-
     // Xoá dữ liệu cũ
-    fs.writeFileSync(currentFilePath, '', 'utf8')
-    fs.writeFileSync(countFilePath, '0', 'utf8')
+    // fs.writeFileSync(currentFilePath, '', 'utf8')
+    // fs.writeFileSync(countFilePath, '0', 'utf8')
     fs.writeFileSync(newPhonetFilePath, '', 'utf8')
     const zeroFile = path.join(staticFolderPath, 'list_phone_0.txt')
 
@@ -80,25 +82,45 @@ app.get('/get-list-phone-active', (req, res) => {
             if (!data.startsWith('disable_')) {
                 const numbers = data.replace(/^disable_/, '').split(',')
                 const firstPhone = numbers.shift()
-                phoneList.push({ port, num: firstPhone, status: 1 })
+
+                // Kiểm tra trạng thái của port
+                const portStatusFilePath = path.join(staticFolderPath, `status_${port}.txt`)
+                const portStatusData = fs.readFileSync(portStatusFilePath, 'utf-8')
+                const portValue = portStatusData.split(',')
+
+                phoneList.push({ port, num: firstPhone, success: portValue[0], failed: portValue[1] })
                 fs.writeFileSync(filePath, 'disable_' + numbers.join(','), 'utf8')
+            }
+
+            if (data.length === 0 || data.trim() === 'disable_') {
+                fs.unlinkSync(filePath)
             }
         }
     })
 
-    const jsonString = JSON.stringify(phoneList, null, 2)
-    fs.writeFileSync(currentFilePath, jsonString, 'utf8')
     res.json(phoneList)
 })
 
 // Reset về đàu
 app.get('/reset', (req, res) => {
     const files = fs.readdirSync(staticFolderPath)
-
+    counter = 0
     files.forEach((file) => {
         const filePath = path.join(staticFolderPath, file)
         fs.writeFileSync(filePath, '', 'utf8')
         console.log(`Cleared content of file: ${file}`)
+
+        // xử lý riêng
+        fs.writeFileSync(currentFilePath, '[]', 'utf8')
+        fs.writeFileSync(countFilePath, '0', 'utf8')
+
+        const match = file.match(/^list_phone_(\d+)\.txt$/)
+        const matchStatus = file.match(/^status_(\d+)\.txt$/)
+        if (match || matchStatus) {
+            const filePa = path.join(staticFolderPath, file)
+            fs.unlinkSync(filePa)
+            console.log(`Deleted file: ${file}`)
+        }
     })
 
     res.send('reset phone thành công')
@@ -106,80 +128,154 @@ app.get('/reset', (req, res) => {
 
 // lấy 1 phone
 app.get('/get-phone', (req, res) => {
-    const data = fs.readFileSync(currentFilePath, 'utf8')
-    if (JSON.parse(data).length === 0) {
-        res.json(-1)
-    } else {
-        const listPhone = JSON.parse(data)
-        console.log('list current phone: ', listPhone)
+    let minPort = Infinity
+    let minPortFileName = ''
+    fs.readdirSync(staticFolderPath).forEach((file) => {
+        const match = file.match(/^list_phone_(\d+)\.txt$/)
+        if (match) {
+            const port = parseInt(match[1], 10)
+            const filePath = path.join(staticFolderPath, file)
 
-        const newPhone = listPhone.shift()
-        console.log('listphone: ', listPhone)
-        fs.writeFileSync(currentFilePath, JSON.stringify(listPhone, null, 2), 'utf8', { flag: 'w' })
+            const data = fs.readFileSync(filePath, 'utf8')
 
-        const newPhoneData = `${newPhone.port}-${newPhone.num}\n`
-        fs.writeFileSync(newPhonetFilePath, newPhoneData, { flag: 'a' })
+            if (!data.startsWith('disable')) {
+                if (port < minPort) {
+                    minPort = port
+                    minPortFileName = file
+                }
+            }
+        }
+    })
 
-        counter += 1
-        console.log(counter)
-
-        fs.writeFileSync(countFilePath, counter.toString(), 'utf8', { flag: 'w' })
-        res.send(`${newPhone.port}-${newPhone.num}`)
+    if (minPort === Infinity) {
+        res.status(404).json({
+            result: -1,
+            message: 'Không có port',
+        })
+        return
     }
+
+    const filePath = path.join(staticFolderPath, minPortFileName)
+    const data = fs.readFileSync(filePath, 'utf8')
+    const listPhones = data.split(',')
+
+    if (listPhones.length === 0) {
+        res.status(404).send({
+            result: -2,
+            mesage: `Không có phone ở port ${minPort}`,
+        })
+        return
+    }
+
+    const firstPhone = listPhones.shift()
+    fs.writeFileSync(filePath, 'disable_' + listPhones.join(','), 'utf8')
+
+    // Ghi đè hoặc tạo mới file
+    const newFilePath = path.join(staticFolderPath, 'new_phone.txt')
+    const newPhoneData = `${minPort}-${firstPhone}\n`
+    fs.writeFileSync(newFilePath, newPhoneData, { flag: 'a' })
+
+    // Ghi đè hoặc tạo mới file count.txt
+    const countFilePath = path.join(staticFolderPath, 'count.txt')
+    counter += 1
+    console.log(counter)
+
+    fs.writeFileSync(countFilePath, counter.toString(), 'utf8', { flag: 'w' })
+
+    res.send(`${minPort}-${firstPhone}`)
 })
 
 // lấy danh sách các phon hiện tại
 app.get('/get-list-current-phone', (req, res) => {
-    const data = fs.readFileSync(currentFilePath, 'utf8')
-    const listPhone = data ? JSON.parse(data) : []
-    res.json(listPhone)
+    const phoneList = []
+    fs.readdirSync(staticFolderPath).forEach((file) => {
+        const match = file.match(/^list_phone_(\d+)\.txt$/)
+        if (match) {
+            const port = match[1]
+            const filePath = path.join(staticFolderPath, file)
+            const data = fs.readFileSync(filePath, 'utf8')
+            if (!data.startsWith('disable_')) {
+                const numbers = data.replace(/^disable_/, '').split(',')
+                const firstPhone = numbers[0]
+
+                // Kiểm tra trạng thái của port
+                const portStatusFilePath = path.join(staticFolderPath, `status_${port}.txt`)
+                const portStatusData = fs.readFileSync(portStatusFilePath, 'utf-8')
+                const portValue = portStatusData.split(',')
+
+                phoneList.push({ port, num: firstPhone, success: portValue[0], failed: portValue[1] })
+                // fs.writeFileSync(filePath, 'disable_' + numbers.join(','), 'utf8')
+            }
+
+            if (data.length === 0 || data.trim() === 'disable_') {
+                fs.unlinkSync(filePath)
+            }
+        }
+    })
+    res.json(phoneList)
 })
 
 // active port lên
-app.get('/active/:port', (req, res) => {
-    const port = req.params.port
-    const fileName = `list_phone_${port}.txt`
-    const filePath = path.join(staticFolderPath, fileName)
+app.get('/active/:port/:status', (req, res) => {
+    try {
+        const port = req.params.port
+        const status = req.params.status
 
-    if (fs.existsSync(filePath)) {
-        let data = fs.readFileSync(filePath, 'utf8')
+        const fileName = `list_phone_${port}.txt`
+        const filePath = path.join(staticFolderPath, fileName)
 
-        // Check if the file is empty or contains only "disable_"
-        if (!data.trim() || data.trim() === 'disable_') {
-            res.status(400).send(`Không có port tương ứng với ${port}`)
-            return
-        }
+        if (fs.existsSync(filePath)) {
+            let data = fs.readFileSync(filePath, 'utf8')
 
-        if (data.startsWith('disable_')) {
-            data = data.substring('disable_'.length)
-            fs.writeFileSync(filePath, data, 'utf8')
-            res.send(`Port ${port} đã được bật`)
+            if (!data.trim() || data.trim() === 'disable_') {
+                res.status(400).send(`Không có port tương ứng với ${port}`)
+                return
+            }
+
+            if (data.startsWith('disable_')) {
+                data = data.substring('disable_'.length)
+                fs.writeFileSync(filePath, data, 'utf8')
+
+                // Ghi vào trạng thái của port
+                const filePortName = `status_${port}.txt`
+                const filePortPath = path.join(staticFolderPath, filePortName)
+                const portValue = fs.readFileSync(filePortPath, 'utf8')
+
+                let countValue = portValue.split(',')
+                let countSucess = parseInt(countValue[0])
+                let countFail = parseInt(countValue[1])
+
+                if (fs.existsSync(filePath) && status) {
+                    if (status === 'success') {
+                        countSucess += 1
+                    } else if (status === 'fail') {
+                        countFail += 1
+                    }
+                }
+                fs.writeFileSync(filePortPath, `${countSucess},${countFail}`, 'utf8', { flag: 'w' })
+                //
+
+                res.send(`Port ${port} đã được bật`)
+            } else {
+                res.status(400).send(`Port ${port} đã được bật, hãy lấy phone trước khi bật lại`)
+            }
         } else {
-            console.log(`Port ${port} is already enabled`)
-            res.status(400).send(`Port ${port} đã được bật`)
+            res.status(400).send(`Port ${port} không tồn tại`)
         }
-    } else {
-        console.log(`Port ${port} does not exist`)
-        res.status(400).send(`Port ${port} không tồn tại`)
+    } catch (error) {
+        console.log('active error', error.message)
+        return null
     }
 })
 
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'uploads/')
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, Date.now() + '-' + file.originalname)
-//     },
-// })
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, 'uploads/')
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
+        cb(null, Date.now() + '-' + file.originalname)
     },
-});
+})
 const upload = multer({ storage: storage })
 app.post('/upload', upload.single('file'), (req, res) => {
     console.log('dô hog', req.file)
@@ -195,15 +291,15 @@ app.post('/upload', upload.single('file'), (req, res) => {
     //     'myfilename.jpeg',
     //     { type: "image/jpeg" }
     //  );
-    const binaryData = Buffer.from(base64Data, 'base64').toString('binary');
+    const binaryData = Buffer.from(base64Data, 'base64').toString('binary')
     //const bufferData = Buffer.from(base64Data, 'base64');
     // Tạo tên tệp và lưu vào thư mục uploads
-    const fileName = Date.now() + '-' + req.file.originalname;
-    const filePath1 = path.join('uploads', fileName);
-    console.log(binaryData);
-    fs.writeFile(filePath1, binaryData, {encoding: 'base64'}, function(err) {
-        console.log('File created',binaryData);
-    });
+    const fileName = Date.now() + '-' + req.file.originalname
+    const filePath1 = path.join('uploads', fileName)
+    console.log(binaryData)
+    fs.writeFile(filePath1, binaryData, { encoding: 'base64' }, function (err) {
+        console.log('File created', binaryData)
+    })
 
     // Lưu dữ liệu nhị phân vào tệp
     //fs.writeFileSync(req.file.path, binaryData);
@@ -240,8 +336,8 @@ app.post('/uploadFromPath', (req, res) => {
         files.forEach((fileName) => {
             const absolutePath = path.join(absoluteFolderPath, fileName)
             if (!fs.existsSync(absolutePath)) {
-                console.error(`File '${fileName}' không tồn tại.`);
-                return;  // Bỏ qua file không tồn tại và tiếp tục với file khác
+                console.error(`File '${fileName}' không tồn tại.`)
+                return // Bỏ qua file không tồn tại và tiếp tục với file khác
             }
             // Gọi API upload
             const uploadApiUrl = `${urlServer}/upload` // Thay đổi thành URL thực tế của API upload
@@ -251,9 +347,9 @@ app.post('/uploadFromPath', (req, res) => {
             // console.log(formDataHeaders);
             // Create a readable stream for the file
             try {
-                fs.readFileSync(absolutePath);
+                fs.readFileSync(absolutePath)
             } catch (error) {
-                return ;
+                return
             }
             const fileStream = fs.createReadStream(absolutePath)
             let fileBuffer = Buffer.from([])
@@ -263,13 +359,13 @@ app.post('/uploadFromPath', (req, res) => {
 
             fileStream.on('end', () => {
                 try {
-                    fs.readFileSync(absolutePath);
+                    fs.readFileSync(absolutePath)
                 } catch (error) {
-                    return ;
+                    return
                 }
                 // Convert the buffer to a base64-encoded string
                 const fileBase64 = fileBuffer.toString('base64')
-                console.log("truyền lên",fileBuffer,fileBase64);
+                console.log('truyền lên', fileBuffer, fileBase64)
                 // Build the form data string
                 const formDataString = `--myboundary\r\nContent-Disposition: form-data; name="file"; filename="${path.basename(
                     absolutePath,
@@ -426,8 +522,8 @@ app.get('/getvoice/:port/:num', (req, res) => {
             result = { ...JSON.parse(result) }
             result.url = result.url.replaceAll('\\', '/')
 
-            pathFileVoice = result.url.replace(urlServer, ".");
-            absolutePathVoice=path.resolve(pathFileVoice)
+            pathFileVoice = result.url.replace(urlServer, '.')
+            absolutePathVoice = path.resolve(pathFileVoice)
             // const fileStream = fs.createReadStream(absolutePathVoice)
             // let fileBuffer = Buffer.from([])
             // fileStream.on('data', (chunk) => {
@@ -458,42 +554,40 @@ app.get('/getvoice/:port/:num', (req, res) => {
             //     .then((response) => {console.log("res->",response); })
             //     .catch ((error) => { console.log("error->",error.response);})
             // })
-            const formData = new FormData();
+            const formData = new FormData()
             formData.append('file', fs.createReadStream(absolutePathVoice), {
                 filename: path.basename(absolutePathVoice),
                 contentType: 'audio/wav', // Replace with the actual MIME type of your file
-              });
-              formData.append('model', 'whisper-1');
-              formData.append('response_format', 'text');
-axios.post('https://api.openai.com/v1/audio/transcriptions', formData, {
-  headers: {
-    //...formData.getHeaders(),
-    'Authorization': 'Bearer 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',
-  },
-})
-  .then(response => {
-      console.log("res->", response.data);
-      result.text = response.data
-      res.json(result)
-  })
-  .catch(error => {
-      console.log("error->", error.response.data);
-      res.json(result)
-  });
-                
-                
+            })
+            formData.append('model', 'whisper-1')
+            formData.append('response_format', 'text')
+            axios
+                .post('https://api.openai.com/v1/audio/transcriptions', formData, {
+                    headers: {
+                        //...formData.getHeaders(),
+                        Authorization: 'Bearer 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',
+                    },
+                })
+                .then((response) => {
+                    console.log('res->', response.data)
+                    result.text = response.data
+                    res.json(result)
+                })
+                .catch((error) => {
+                    console.log('error->', error.response.data)
+                    res.json(result)
+                })
         }
     }
 
     // Kiểm tra xem port có tồn tại trong file log không
 
-   
     // } else {
     //   // Nếu không tồn tại, trả về thông báo lỗi
     //   res.status(404).json({ error: 'Không tìm thấy số điện thoại cho port này.' });
     // }
 })
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000')
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 })
